555558b11814267850c7d4a158be40a944884386
MikeTroianello <mike.troianello@gmail.com>
2021-12-30 16:36:24 -0800

    moved routes

--- models/User.model.js
+++ models/User.model.js
@@ -1,11 +1,12 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      unique: true,
+      required: true,
     },
     password: String,
   },
@@ -15,6 +16,6 @@ const userSchema = new Schema(
   }
 );
 
-const User = model("User", userSchema);
+const User = model('User', userSchema);
 
 module.exports = User;
