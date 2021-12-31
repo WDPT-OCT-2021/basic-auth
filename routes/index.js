0fe5169e6d8fd04cc9d5716ef6eef3bf6078eb11
MikeTroianello <mike.troianello@gmail.com>
2021-12-30 17:46:27 -0800

    added profile page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const isLoggedIn = require('../middleware/isLoggedIn');
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/profile', isLoggedIn, (req, res, next) => {
+  res.render('profile', { name: req.user.username });
 });
 
 module.exports = router;
